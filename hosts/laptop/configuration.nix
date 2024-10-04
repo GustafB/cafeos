b556a87fababecf31b363d14053a35da16f47bc6
gustafb <gustaf.brostedt@gmail.com>
2024-10-04 05:53:32 +0200

    cleanup conf

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -10,15 +10,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
-  networking.hostName = host; # Define your hostname.
+  networking.hostName = host; 
 
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Stockholm";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -152,7 +149,6 @@
   
   security.pam.services.hyprlock = {};
   
-  
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
@@ -166,34 +162,6 @@
       nvidiaBusId = "PCI:1:0:0";
     };
   };
-  
-  
-    # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
-
+  system.stateVersion = "24.05"; 
 }
