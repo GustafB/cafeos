2b16793d9111d2c7a11327f340a7f82a8533f494
gustafb <gustaf.brostedt@gmail.com>
2024-10-04 06:38:19 +0200

    add resize font to kitty

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -22,30 +22,32 @@ in
     programs.git = import ../../config/git.nix { inherit pkgs lib gitUsername gitEmail; };
 
     programs = {
-        home-manager.enable = true;
-        kitty = {
-            enable = true;
-            package = pkgs.kitty;
-            settings = {
-                scrollback_lines = 2000;
-                wheel_scroll_min_lines = 1;
-                window_padding_width = 4;
-                confirm_os_window_close = 0;
-            };
-            extraConfig = ''
-                tab_bar_style fade
-                tab_fade 1
-                active_tab_font_style   bold
-                inactive_tab_font_style bold
-            '';
+      home-manager.enable = true;
+      kitty = {
+        enable = true;
+        package = pkgs.kitty;
+        settings = {
+          scrollback_lines = 2000;
+          wheel_scroll_min_lines = 1;
+          window_padding_width = 4;
+          confirm_os_window_close = 0;
+	};
+        extraConfig = ''
+          tab_bar_style fade
+          tab_fade 1
+          active_tab_font_style   bold
+          inactive_tab_font_style bold
+          map alt+minus change_font_size all -1
+          map alt+equal change_font_size all +1
+        '';
         };
 	starship = {
 	  enable = true;
 	  package = pkgs.starship;
 	};
         zsh = {
-            enable = true;
-            enableCompletion = true;
+          enable = true;
+          enableCompletion = true;
         };
     };
 }
