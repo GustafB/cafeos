9570bfa307076e9462484acf44b6f332ca2dadbb
gustafb <gustaf.brostedt@gmail.com>
2024-10-07 12:36:45 +0200

    add python and jupyter

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, username, host, lib, ... }:
+{ pkgs, config, username, host, lib, ... }:
 let
   inherit (import ./variables.nix) gitUsername gitEmail gitPublicKey;
 in
@@ -14,12 +14,36 @@ in
         ../../config/rofi.nix
     ];
 
-    home.packages = [
+    home.packages = with pkgs; [
       (import ../../scripts/rofi-launcher.nix { inherit pkgs; })
       (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" "DroidSansMono" ]; })
+        (python311.withPackages (
+                ps:
+                    with ps; [
+                    setuptools
+                    jupyter
+                    jupyterlab
+                    ipython
+                    ipykernel
+                    # DS
+                    matplotlib
+                    numpy
+                    plotly
+                    # scikit-learn-extra
+                    scipy
+                    seaborn
+                    pandas
+                    # formatter
+                    black
+                    ruff
+                    # other
+                    virtualenv
+                    ])
+            )
     ];
 
     programs.git = import ../../config/git.nix { inherit pkgs lib gitUsername gitEmail gitPublicKey; };
+
 
     programs = {
       home-manager.enable = true;
@@ -41,20 +65,16 @@ in
           map alt+equal change_font_size all +1
         '';
         };
-	starship = {
-	  enable = true;
-	  package = pkgs.starship;
-	};
-        zsh = {
-          enable = true;
-          enableCompletion = true;
+    zsh = {
+      enable = true;
+      enableCompletion = true;
 	  # load personal config and enable emacs keybinds for cmdline
 	  initExtra = ''
 	  fastfetch
 	  if [[ -f $HOME/.zshrc-personal ]]; then
 	    source $HOME/.zshrc-personal
 	  fi
-          bindkey -e
+      bindkey -e
 	  '';
 	  shellAliases = {
 	    sv = "sudo nvim";
@@ -69,6 +89,13 @@ in
             "..4" = "cd ../../../..";
 	    rebuild = "sudo nixos-rebuild switch --flake ~/cafeos/#laptop";
 	  };
-        };
     };
+    fzf = {
+        enable = true;
+    };
+    starship = {
+        enable = true;
+        package = pkgs.starship;
+    };
+  };
 }
