7b9f0740d9867f946a0e23a1e8f5ebfa68577280
GustafB <gustaf.brostedt@gmail.com>
2024-11-04 22:42:38 +0100

    move starship into home

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -16,6 +16,7 @@
     ./users.nix
     ../../modules/programs/hyprland.nix
     ../../modules/nvidia.nix
+    ../../modules/programs/zsh.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
@@ -108,76 +109,6 @@
     _1password-gui = {
       enable = true;
       polkitPolicyOwners = [ "{$username}" ];
-    };
-    starship = {
-      enable = true;
-      package = pkgs.starship;
-      settings = {
-        add_newline = false;
-        buf = {
-          symbol = " ";
-        };
-        c = {
-          symbol = " ";
-        };
-        directory = {
-          read_only = " 󰌾";
-        };
-        docker_context = {
-          symbol = " ";
-        };
-        fossil_branch = {
-          symbol = " ";
-        };
-        git_branch = {
-          symbol = " ";
-        };
-        golang = {
-          symbol = " ";
-        };
-        hg_branch = {
-          symbol = " ";
-        };
-        hostname = {
-          ssh_symbol = " ";
-        };
-        lua = {
-          symbol = " ";
-        };
-        memory_usage = {
-          symbol = "󰍛 ";
-        };
-        meson = {
-          symbol = "󰔷 ";
-        };
-        nim = {
-          symbol = "󰆥 ";
-        };
-        nix_shell = {
-          symbol = " ";
-        };
-        nodejs = {
-          symbol = " ";
-        };
-        ocaml = {
-          symbol = " ";
-        };
-        package = {
-          symbol = "󰏗 ";
-        };
-        python = {
-          symbol = " ";
-        };
-        rust = {
-          symbol = " ";
-        };
-        swift = {
-          symbol = " ";
-        };
-        zig = {
-          symbol = " ";
-        };
-      };
     };
     thunar = {
       enable = true;
---
+++ modules/programs/zsh.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+{
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      promptInit = ''
+        eval "$(${lib.getExe pkgs.starship} init zsh)"
+      '';
+    };
+  };
+}
