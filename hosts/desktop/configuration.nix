d5189bd1e70c0159e36f7ce6781bd92a7cfdbf63
GustafB <gustaf.brostedt@gmail.com>
2024-11-05 09:57:37 +0100

    setup security and il8

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -10,6 +10,11 @@
     ./hardware.nix
     ./users.nix
 
+    ../../modules/config/il8.nix
+    ../../modules/config/xdg.nix
+    ../../modules/config/security.nix
+
+    ../../modules/hardware/graphics.nix
     ../../modules/hardware/nvidia.nix
     ../../modules/hardware/bluetooth.nix
 
@@ -27,20 +32,6 @@
 
   time.timeZone = "Europe/Stockholm";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sv_SE.UTF-8";
-    LC_IDENTIFICATION = "sv_SE.UTF-8";
-    LC_MEASUREMENT = "sv_SE.UTF-8";
-    LC_MONETARY = "sv_SE.UTF-8";
-    LC_NAME = "sv_SE.UTF-8";
-    LC_NUMERIC = "sv_SE.UTF-8";
-    LC_PAPER = "sv_SE.UTF-8";
-    LC_TELEPHONE = "sv_SE.UTF-8";
-    LC_TIME = "sv_SE.UTF-8";
-  };
-
   # enable nvidia drivers
   drivers.nvidia.enable = true;
 
@@ -54,8 +45,8 @@
   environment.systemPackages = with pkgs; [
     vim
     wget
-    killall
     curl
+    killall
     jq
     kitty
     git
@@ -91,59 +82,15 @@
   #   };
   # };
 
-  # grapics
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal
-    ];
-    configPackages = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal
-    ];
-  };
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = false;
-  };
-
   services = {
-    xserver = {
-      enable = false;
-      xkb = {
-        layout = "us";
-        variant = "";
-      };
-    };
+    # xserver = {
+    #   enable = false;
+    #   xkb = {
+    #     layout = "us";
+    #     variant = "";
+    #   };
+    # };
     libinput.enable = true;
-  };
-
-  security = {
-    pam.services.hyprlock = { };
-    rtkit.enable = true;
-    polkit = {
-      enable = true;
-      extraConfig = ''
-        polkit.addRule(function(action, subject) {
-              if (
-                  subject.isInGroup("users")
-                  && (
-                    action.id == "org.freedesktop.login1.reboot" ||
-                    action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-                    action.id == "org.freedesktop.login1.power-off" ||
-                    action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-                  )
-              )
-              {
-                return polkit.Result.YES;
-              }
-          })
-      '';
-    };
   };
 
   system.stateVersion = "24.05";
---
+++ modules/config/security.nix
@@ -0,0 +1,25 @@
+{
+  security = {
+    pam.services.hyprlock = { };
+    rtkit.enable = true;
+    polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+              if (
+                  subject.isInGroup("users")
+                  && (
+                    action.id == "org.freedesktop.login1.reboot" ||
+                    action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+                    action.id == "org.freedesktop.login1.power-off" ||
+                    action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+                  )
+              )
+              {
+                return polkit.Result.YES;
+              }
+          })
+      '';
+    };
+  };
+}
