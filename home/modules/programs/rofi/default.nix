b56ac1a778037380f60393e676c38d2806fa0175
GustafB <gustaf.brostedt@gmail.com>
2024-11-06 18:22:31 +0100

    fix rofi pkg

--- home/modules/programs/rofi/default.nix
+++ home/modules/programs/rofi/default.nix
@@ -1,6 +1,25 @@
+{ pkgs, ... }:
 {
   imports = [
-    ./rofi.nix
     ./scripts/rofi-launcher.nix
   ];
+
+  programs = {
+    rofi = {
+      enable = true;
+      package = pkgs.rofi-wayland;
+      extraConfig = {
+        modi = "drun,filebrowser,run";
+        show-icons = true;
+        icon-theme = "Papirus";
+        location = 0;
+        font = "JetBrainsMono Nerd Font Mono 12";
+        drun-display-format = "{icon} {name}";
+        display-drun = " Apps";
+        display-run = " Run";
+        display-filebrowser = " File";
+      };
+
+    };
+  };
 }
